10c516a6d2da65e50ec7406fd9a41de61a4bb818
ericgalbarn <ericgalbarn@gmail.com>
2024-10-18 16:11:55 +0700

    Finalizing the UI

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const sendButton = document.querySelector(".btn-outline-secondary");
-
-// Thực hiện sự kiện khi người dùng click vào nút
-sendButton.addEventListener("click", function () {
-  alert("Button clicked!");
-});
